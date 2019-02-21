399774ebca76692f7785b73f9def9eec10cfff19
nikhilbalyan <nbalyan10@gmail.com>
2019-02-21 18:41:38 +0530

    initial commit

---
+++ skeleton-web-services/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
