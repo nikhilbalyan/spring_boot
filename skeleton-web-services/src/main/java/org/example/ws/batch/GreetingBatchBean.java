be5b0d1bea29737c81d0d0dbb9499a242fc61759
nikhilbalyan <nbalyan10@gmail.com>
2019-02-27 22:34:38 +0530

    add scheduling logic in place for the 3 types of scheduling

--- skeleton-web-services/src/main/java/org/example/ws/batch/GreetingBatchBean.java
+++ skeleton-web-services/src/main/java/org/example/ws/batch/GreetingBatchBean.java
@@ -24,7 +24,7 @@ public class GreetingBatchBean {
 	
 	// scheduler that runs at 0 and 30 secs of every min of every hour of every day
 //	@Scheduled(cron="0.30 * * * * *")
-	@Scheduled(fixedDelay = 1000)
+//	@Scheduled(fixedDelay = 1000)
 	public  void cronJob() {
 		logger.info("> cronJob");
 		
@@ -36,4 +36,45 @@ public class GreetingBatchBean {
 		logger.info("< cronJob");
 	}
 	
+	@Scheduled(
+			initialDelay = 5000,
+			fixedRate = 15000)
+	public void fixedRateJobWithInitialDelay() {
+		logger.info("> fixedRateJobWithInitialDelay");
+		
+		// Add scheduled logic here
+		// Simulate Job processing time
+		
+		long pause = 5000;
+		long start = System.currentTimeMillis();
+		do {
+			if ( start + pause < System.currentTimeMillis()) { // it will add 5 seconds pause before
+															   // the execution of program further
+				break;
+			}
+		} while (true);
+		logger.info("processing time was {} seconds", pause/1000);
+		logger.info("< fixedRateJobWithInitialDelay");
+	}
+	
+	
+	@Scheduled(
+			initialDelay = 5000,
+			fixedDelay = 15000)
+	public void fixedDelayJobWithInitialDelay() {
+		logger.info("> fixedDelayJobWithInitialDelay");
+		
+		
+		// Add scheduled logic here
+		// simulate job processing time
+		long pause = 5000;
+		long start = System.currentTimeMillis();
+		do {
+			if (start + pause < System.currentTimeMillis()) {
+				break;
+			}
+		} while (true);
+		logger.info("processing time was {} seconds.", pause / 1000);
+		logger.info("< fixedDelayJobWithInitialDelay");
+	}
 }
